b8d8adf5832a029d722f78f6de427b7e4e5cba4f
Radytin <Kreetta.hamalainen@helsinki.fi>
2017-02-02 11:14:39 +0200

    Luokkakaavio ja testiluokkien luontia

---
+++ Awesomechess/src/test/java/com/chess/boardtest/BoardUtilsTest.java
@@ -0,0 +1,35 @@
+package com.chess.boardtest;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BoardUtilsTest {
+	
+	@BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+	@Test
+	public void test() {
+		
+	}
+
+}
---
+++ Awesomechess/src/test/java/com/chess/boardtest/TileTest.java
@@ -0,0 +1,49 @@
+package com.chess.boardtest;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.chess.chessboard.Tile;
+import com.chess.chessboard.Tile.emptyTile;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TileTest {
+	
+		
+		@BeforeClass
+	    public void setUpClass() { 
+	    }
+
+	    @AfterClass
+	    public void tearDownClass() {  
+	    }
+
+	    @Before
+	    public void setUp() { 
+	    }
+
+	    @After
+	    public void tearDown() { 
+	    }
+
+	
+
+	  
+		
+	   
+	   
+	   
+	   }
+	
+
