aa2be234891b97599170a66983f519accb9ec18b
Radytin <Kreetta.hamalainen@helsinki.fi>
2017-02-13 10:58:23 +0200

    Jatkettu Board builderiä, muutettu palojen constructoreita järkevämmäksi

--- Awesomechess/src/main/java/com/chess/chessboard/Board.java
+++ Awesomechess/src/main/java/com/chess/chessboard/Board.java
@@ -4,7 +4,13 @@ import java.util.List;
 import java.util.Map;
 
 import com.chess.chessengine.Alliance;
+import com.chess.piece.Bishop;
+import com.chess.piece.King;
+import com.chess.piece.Knight;
+import com.chess.piece.Pawn;
 import com.chess.piece.Piece;
+import com.chess.piece.Queen;
+import com.chess.piece.Rook;
 import com.google.common.collect.ImmutableList;
 
 public class Board {
@@ -31,10 +37,46 @@ public class Board {
 		
 	}
 	
-	public static Board createStandardBoard(){
-		return null;
-		
-	}
+	public static Board createStandardBoard() {
+        final Builder builder = new Builder();
+        // Black pieces
+        builder.setPiece(new Rook(Alliance.BLACK, 0));
+        builder.setPiece(new Knight(Alliance.BLACK, 1));
+        builder.setPiece(new Bishop(Alliance.BLACK, 2));
+        builder.setPiece(new Queen(Alliance.BLACK, 3));
+        builder.setPiece(new King(Alliance.BLACK, 4));
+        builder.setPiece(new Bishop(Alliance.BLACK, 5));
+        builder.setPiece(new Knight(Alliance.BLACK, 6));
+        builder.setPiece(new Rook(Alliance.BLACK, 7));
+        builder.setPiece(new Pawn(Alliance.BLACK, 8));
+        builder.setPiece(new Pawn(Alliance.BLACK, 9));
+        builder.setPiece(new Pawn(Alliance.BLACK, 10));
+        builder.setPiece(new Pawn(Alliance.BLACK, 11));
+        builder.setPiece(new Pawn(Alliance.BLACK, 12));
+        builder.setPiece(new Pawn(Alliance.BLACK, 13));
+        builder.setPiece(new Pawn(Alliance.BLACK, 14));
+        builder.setPiece(new Pawn(Alliance.BLACK, 15));
+        // White pieces
+        builder.setPiece(new Pawn(Alliance.WHITE, 48));
+        builder.setPiece(new Pawn(Alliance.WHITE, 49));
+        builder.setPiece(new Pawn(Alliance.WHITE, 50));
+        builder.setPiece(new Pawn(Alliance.WHITE, 51));
+        builder.setPiece(new Pawn(Alliance.WHITE, 52));
+        builder.setPiece(new Pawn(Alliance.WHITE, 53));
+        builder.setPiece(new Pawn(Alliance.WHITE, 54));
+        builder.setPiece(new Pawn(Alliance.WHITE, 55));
+        builder.setPiece(new Rook(Alliance.WHITE, 56));
+        builder.setPiece(new Knight(Alliance.WHITE, 57));
+        builder.setPiece(new Bishop(Alliance.WHITE, 58));
+        builder.setPiece(new Queen(Alliance.WHITE, 59));
+        builder.setPiece(new King(Alliance.WHITE, 60));
+        builder.setPiece(new Bishop(Alliance.WHITE, 61));
+        builder.setPiece(new Knight(Alliance.WHITE, 62));
+        builder.setPiece(new Rook(Alliance.WHITE, 63));
+        builder.setMoveMaker(Alliance.WHITE);
+        //Assemble the board
+        return builder.build();
+}
 	
 	public static class Builder {
 		
